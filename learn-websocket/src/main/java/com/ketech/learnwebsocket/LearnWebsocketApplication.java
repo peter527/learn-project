3ebb8e0b5d727b69adbad233034f4ad2d29c9997
wuguohua <wugh@ketech.com.cn>
2017-10-23 13:08:15 +0800

    创建了websocket学习项目

---
+++ learn-websocket/src/main/java/com/ketech/learnwebsocket/LearnWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.ketech.learnwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnWebsocketApplication.class, args);
+	}
+}
